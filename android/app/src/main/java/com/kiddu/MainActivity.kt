1f9aeb505f0b47a053cb7cd7ef9dadce6487d12d
Muhammad Umer Mujtaba <umermujtaba16@gmail.com>
2026-04-29 17:28:20 +0500

    Initial Commit

---
+++ android/app/src/main/java/com/kiddu/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.kiddu
+
+import android.os.Bundle // ✅ ADD THIS
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
+import com.zoontek.rnbootsplash.RNBootSplash
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "Kiddu"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return ReactActivityDelegate(this, mainComponentName)
+    }
+  // ✅ ADD THIS BLOCK
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    RNBootSplash.init(this, R.style.BootTheme)
+    super.onCreate(savedInstanceState)
+  }
+}
---
+++ ios/Kiddu/AppDelegate.swift
@@ -0,0 +1,42 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+import Firebase
+import RNBootSplash
+
+@main
+class AppDelegate: RCTAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    self.moduleName = "Kiddu"
+    self.dependencyProvider = RCTAppDependencyProvider()
+
+    // You can add your custom initial props in the dictionary below.
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = [:]
+
+    let success = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    if success {
+     RNBootSplash.initWithStoryboard("BootSplash", rootView: self.window.rootViewController?.view)
+    }
+
+    return success
+  }
+
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
